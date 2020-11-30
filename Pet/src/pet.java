5613d10c465d4507b9c7db55b8015653f2637dbd
Nicblue939 <73443833+Nicblue939@users.noreply.github.com>
2020-11-30 13:52:07 -0500

    Add files via upload

---
+++ Pet/src/pet.java
@@ -0,0 +1,66 @@
+
+public class pet {
+	 
+	private String name;
+	private int age;
+	private String location;
+	private String type;
+
+	 public pet(String name, int age, String location, String type) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.location = location;
+		this.type = type;
+	}
+
+	 public String getName() {
+		return name;
+	}
+
+	 public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String toString() {
+		return "pet [name=" + name +", age=" + age +", location=" + location + ", type=" + type + "]";
+	}
+	
+
+
+
+	public static void main(String[] args) {
+		 		 
+		
+		
+		
+	}
+}	
+		
+		
+
+
